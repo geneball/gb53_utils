e6611ef06a27cec888c223add108e1c71fd01e33
Gene Ball <geneball@gmail.com>
2015-11-15 11:29:41 -0800

    initial

---
+++ gb53_utils.js
@@ -0,0 +1,394 @@
+/**
+ * gb53.net utils module - 
+ * @module gb53_utils
+ */
+'use strict';
+
+// modules packaged into vendor_bundle.js
+var $ = require('jquery');
+var moment = require('moment');
+var sprintf = require('sprintf');
+var expect = require('expect');		// for verify()
+
+//var Utils = require('./utils'), PI = Utils.PI, DEG = Utils.DEG, asBool = Utils.asBool, asNum = Utils.asNum, asInt = Utils.asInt, selBool = Utils.selBool, selNum = Utils.selNum, selInt = Utils.selInt, angleWrap = Utils.angleWrap, clamp = Utils.clamp, pctClamp = Utils.pctClamp;
+var Log 	= require('../bkts/js/log'); // Log. e() w() d() i() v() V() W() WF() C() show()
+
+var Version = 'gb53_utils.js 15-Nov-15';
+
+/** get Version string
+ * @returns {string} version as 'filename.js date'
+ */
+exports.Version = 
+	function Version(){ 
+		return Version; 
+	};
+exports.init = 
+	function init() {
+		Log.i("version: %s -- gb53.net utility functions", Version);
+		var methods = "asBool,asNum,asInt,clamp,pctClamp,angleWrap,asLiteral,qindexOf,extract,verify".split(',');
+		for (var i in methods )
+			Log.d('-%s: DISABLE Util.%s', methods[i], methods[i] );
+	}
+    
+/**
+ * shorthand for Math.PI
+ */
+exports.PI =	Math.PI;
+
+/**
+ * conversion factor for angles in degrees - multiply to convert to radians
+ *   e.g.  180*DEG  =  PI radians
+ */
+exports.DEG = 	Math.PI/180.0;
+
+var asNum = 
+/** 
+ * return numeric value from string or value
+ * @param {var} value - value to be interpreted as number
+ * @param {Number} [default_value] - optional default value, if value isn't recognized
+ * @param {Number} [min_value] - optional minimum legal value, clamped to this
+ * @param {Number} [max_value] - optional maximum legal value, clamped to this
+ * @param {Boolean} [asInt] - optional, if true, result is truncated to integer
+ * @returns {Number}
+ */
+exports.asNum = 
+    function asNum(val, def_value, min, max, asInt){
+    	Log.Call('asNum,val,def_value,min,max,asInt', Array.prototype.slice.call(arguments));
+    	if(def_value==undefined) def_value = 0;
+	if(min==undefined)  min = -Number.MAX_VALUE; 
+    	if(max==undefined)  max = Number.MAX_VALUE; 
+	if(asInt==undefined) asInt = false;
+		
+	if (val=='') 
+	    val = def_value;
+	val = +val;
+	if (isNaN(val)) 
+	    val = def_value;
+		
+	if (asInt) 
+	    val = Math.trunc(val);
+	return clamp(val, min, max);
+}
+
+/** 
+ * return integer value from string or value
+ * @param {var} value - value to be interpreted as number
+ * @param {Number} [default_value] - optional default value, if value isn't recognized
+ * @param {Number} [min_value] - optional minimum legal value, clamped to this
+ * @param {Number} [max_value] - optional maximum legal value, clamped to this
+ * @returns {Number}
+ */
+exports.asInt =
+	function asInt(val, def_value, min, max){
+		Log.Call('asInt,val,def_value,min,max', Array.prototype.slice.call(arguments));
+		return asNum( val, def_value, min, max, true );
+}
+
+var asBool =
+/** 
+ * return true/false from value or string
+ *   recognizes true, false, 't', 'f', 1, 0, 'on', 'off',
+ *   'set', 'unset', 'active', 'inactive',
+ *   and if 'current_value' is passed: 'toggle', 'switch', '-'
+ * @param {var} val - value to be interpreted as boolean
+ * @param {bool} [current_value] - optional current boolean, which can be 'toggled'
+ * @returns {Boolean}
+ */
+exports.asBool = 
+	function asBool(val, current_value){
+		Log.Call('asBool,val,current_value', Array.prototype.slice.call(arguments));
+		if (typeof val != 'string'){
+		    if (val===true  || val===1) return true;
+		    if (val===false || val===0) return false;
+		    Log.e('asBool: unrecognized literal "%s"', val.toString());
+		    return 'ERR';
+		    Log.e('asBool: unrecognized value %s: %s', typeof val, val);
+		    return 'ERR';
+		}
+		var bval = sprintf('.%s.', val.toLowerCase());
+		
+		var on = '.on.true.t.1.set.active.';	// recognized names for 'true'
+		if (on.indexOf(bval) >= 0) return true;
+		
+		var off = '.off.false.f.0.unset.reset.inactive.';   // recognized names for 'false'
+		if (off.indexOf(bval) >= 0) return false;
+		
+		var tog = '.toggle.switch.-.';   // recognized values for 'toggle' -- current_value must be defined
+		if (tog.indexOf(bval) >= 0){
+		    if (current_value!==true && current_value!==false){
+		    	Log.e('asBool: toggle-- current value not boolean--  %s: %s', typeof val, val);
+		    	return 'ERR';
+		    }
+		    return !current_value;
+		}
+		Log.e('asBool: unrecognized string "%s"', val);
+		return 'ERR';
+    }
+
+/** 
+ * return numeric value from jQuery selector
+ * @param {string} sel - jQuery selector of field, e.g. '#inputID'
+ * @param {Number} [default_value] - optional default value, if selector val() isn't recognized
+ * @returns {Number}
+ */
+exports.selNum =
+	function selNum(sel, default_value){
+		Log.Call('selNum,sel,default_value', Array.prototype.slice.call(arguments));
+		return asNum( $(sel).val(), default_value );
+}
+
+/** 
+ * return integer value from jQuery selector
+ * @param {string} sel - jQuery selector of field, e.g. '#inputID'
+ * @param {Number} [default_value] - optional default value, if selector val() isn't recognized
+ * @returns {Number}
+ */
+exports.selInt =
+	function selInt(sel, default_value, min, max){
+		Log.Call('selInt,sel,default_value,min,max', Array.prototype.slice.call(arguments));
+		return asNum( $(sel).val(), default_value, min, max, true );
+}
+
+/** 
+ * return boolean value from jQuery selector
+ * @param {string} sel - jQuery selector of field, e.g. '#inputID'
+ * @returns {Boolean}
+ */
+exports.selBool =
+	function selBool(sel){
+		Log.Call('selBool,sel', Array.prototype.slice.call(arguments));
+		return asBool( $(sel).val() );
+}
+
+/** 
+ * collapse angle in degrees to [0..360)
+ * @param {Number} degrees - angle value in degrees
+ * @returns {Number} - in range 0 upto 360
+ */
+exports.angleWrap = 
+    function angleWrap(deg){ 
+		Log.Call('angleWrap,deg', Array.prototype.slice.call(arguments));
+		while (deg<0) deg += 360;
+		while (deg>=360) deg -= 360;
+		return deg;
+}
+
+var clamp =
+/** 
+ * limit value to range
+ * @param {Number} value - numeric value
+ * @param {Number} min - minimum allowed value
+ * @param {Number} max - maximum allowed value
+ * @returns {Number} - in range min..max
+ */
+exports.clamp = 
+    function clamp(v, min,max){ 
+		Log.Call('clamp,v,min,max', Array.prototype.slice.call(arguments));
+    	if(typeof v != 'number' || typeof min != 'number' || typeof max != 'number')
+    	    return sprintf("ERR[clamp: bad args %s,%s,%s]",typeof v, typeof min, typeof max);
+    	if ( v < min ) return min;
+    	if ( v > max ) return max;
+    	return v;
+}
+
+/** 
+ * map percentage value to range
+ * pctClamp(30, 0,100) => 30
+ * pctClamp(-10, 40,60) => 40
+ * @param {Number} pct - percentage in [0..100]
+ * @param {Number} min - minimum value
+ * @param {Number} max - maximum value
+ * @returns {Number} - in range [min..max] based on 'pct'
+ */
+exports.pctClamp = 
+    function pctClamp(pct, min,max){ 
+		Log.Call('pctClamp,pct,min,max', Array.prototype.slice.call(arguments));
+		return clamp(min + clamp(pct,0,100)*(max-min)/100, min,max); 
+}
+
+/**
+ * display string for Babylon camera
+ * @param {Object} Babylon camera
+ * @returns {string} shows camera position & target
+ */
+exports.showCam =
+    function showCam(cam){ 
+	if (cam==null) return '';
+		
+    	return sprintf('(%5.2f, %5.2f, %5.2f) -> (%5.2f, %5.2f, %5.2f)', 
+    		cam.position.x, cam.position.y, cam.position.z, 
+    		cam.target.x, cam.target.y, cam.target.z); 
+}
+
+var extract =
+/**
+ * extracts specified substring
+ * extract( 'abc( def ) g',   0, '(' ) => 'abc'
+ * extract( 'abc( def ) g', '(', '(' ) => 'def'
+ * extract( 'abc( def ) g', 'd'      ) => 'ef ) g'
+ * extract( 'abc( def ) g',   0, 'd' ) => 'abc('
+ * extract( ' fn() => 0 ', '(', ')' ) 	=> '' 
+ * extract( ' fn(a) => 0 ', '(', ')' ) 	=> 'a'
+ * extract( ' fn( a, b, c) => 0 ', '(', ')' ) => 'a, b, c' 
+ * extract( ' fn( a, b, c) => 0 ',   0, '(' ) => 'fn'
+ * extract( ' fn ( a, b, c) => 0 ',  0, '(' ) => 'fn'
+ * extract( ' fn ( a, b, c) => 0 ',  0, '(' ) => 'fn'
+ * extract( ' fn( a, b, c) => 0 ',  '=>'    ) => '0'
+ * @param {String} source - source string
+ * @param {Number|String} start - starting position or marker
+ * @param {Number|String} end - ending position or marker
+ * @returns {string} returns trimmed string from start..end  
+ */
+exports.extract = 
+    function extract(s, min,max){
+	Log.Call('extract,s,min,max', Array.prototype.slice.call(arguments));
+	if (typeof min==='string'){
+	    var p = qindexOf(s, min);
+	    if (p<0) return null;
+	    min = p + min.length;
+	}
+	if (max==undefined)
+	    max = s.length; 
+	else if (typeof max==='string'){
+	    var p = qindexOf(s, max, min);
+	    if (p<0) return null;
+	    max = p;
+	}
+	Log.e("extract: return '%s'[%d..%d] = '%s'", s,min,max, s.substring(min,max));
+	return s.substring(min, max).trim();
+}
+var qsplit = 
+/**
+ * returns array of 'delim' separated substrings, outside of quoted substrings
+ * @param {String} source - source string
+ * @param {String} delim - separator
+ * @returns {Array} of substrings  
+ */
+exports.qsplit = 
+	function qsplit(s, delim){
+		Log.Call('qsplit,s,delim', Array.prototype.slice.call(arguments));
+		var res = [], strt = 0;
+		var p = qindexOf(s,delim, strt);
+		while (p>=0){
+			res.push( s.substring(strt,p).trim() );
+			Log.d('qsplit: + "%s"', res[res.length-1]);
+			strt = p + delim.length;
+			p = qindexOf(s,delim, strt);
+		}
+		res.push( s.substr(strt));
+		Log.d('qsplit: +."%s"', res[res.length-1]);
+		return res;
+}
+
+var qindexOf =
+/**
+ * returns index of specified substring, outside of quoted substrings
+ * @param {String} source - source string
+ * @param {String} substr - substring to search for
+ * @param {number} optional start - index to begin search
+ * @returns {Number} returns index of 1st occurrence, or -1 if not found  
+ */
+exports.qindexOf = 
+	function qindexOf(s, s2, strt){
+		Log.Call('qindexOf,s,s2,strt', Array.prototype.slice.call(arguments));
+		if (strt==undefined) strt = 0;
+		var instr = false, qchar = '';
+		var sch = s2.charAt(0);
+		for (var i=strt; i<s.length; i++){
+			var ch = s.charAt(i);
+			if (instr){
+				if (ch==qchar) instr = false;
+			} else if (ch=="'" || ch=='"'){
+				instr = true; qchar = ch;
+			} else if (ch==sch){
+				if (s.substr(i,s2.length)==s2)
+					return i;
+			}
+		}
+		Log.d('qindexOf: no "%s" found in "%s":%d', s2,s,strt);
+		return -1;
+}
+
+var asLiteral =
+/** converts quoted strings to strings, unquoted strings to values
+ * @param {String}  value definition
+ * @returns {string} literal value 
+ */
+exports.asLiteral = 
+    function asLiteral( s ){
+		Log.Call('asLiteral,s', Array.prototype.slice.call(arguments));
+    	if (typeof s !== 'string') 
+    	    return s;  // already non-string
+    	s = s.trim();
+    	var fr = s.charAt(0), bk = s.charAt(s.length-1);
+    	if (fr==bk && (fr=='"' || fr=="'")) 
+    	    return s.substr(1,s.length-2).trim();	// unquote, then return string
+    	
+    	if ( s=='true' ) return true;
+    	if ( s=='false' ) return false;
+    	if ( s=='null' ) return null;
+    	if ( s=='' || s=='undefined' ) return undefined;
+    	if ( s=='NaN' ) return 1/0;	// NaN
+
+    	var v = +s;
+    	//console.log('aL: %s:%d', typeof v, v);
+    	if ( !isNaN(v) ) return v;
+    	Log.e('asLiteral: "%s" unrecognized', s);
+    	return 'ERR['+s+']';
+}
+
+var verify = 
+/**
+ * verify test expression
+ * e.g.:  " nm(0,'b',3.5,false) => 8 " translates to it(descr, function(){ expect( fn(0, 'b', 3.5, false).toEqual(8); });
+ *    ==> as toBe,
+ *   throws=>  as toThrow()
+ * @param {String} descr - test description as in examples
+ * @param {Function} fn - javascript function to call
+ */
+exports.verify = 
+    function verify(vstr, actualfn){
+		Log.Call('verify,vstr,fn', Array.prototype.slice.call(arguments));
+    	var fname = extract(vstr, 0,'(' );
+    	var argstr = extract(vstr, '(', ')' );
+    	if (argstr==null) { Log.e('verify: no () found in "%s"',vstr); return; }
+    	var args = argstr==null? [] : qsplit(argstr, ',');
+    	for(var i=0; i<args.length; i++){
+    	    args[i] = asLiteral(args[i]);
+    	    Log.d('verify: arg[%d]=%s', i,args[i]);
+    	}
+    	
+    	var result = asLiteral( extract(vstr, '=>' ) );	
+    	Log.d('verify: got %s', actualfn.apply(this, args));
+    	var op = qindexOf(vstr,'==>')>0? '==>' : qindexOf(vstr,'throws=>')>0? 'throws=>' : '=>';
+    	switch (op){ 
+		case "=>":
+			it(vstr, function(){ expect( actualfn.apply( this, args )).toEqual( result ) });
+			break;
+		case "==>":
+			it(vstr, function(){ expect( actualfn.apply( this, args )).toBe( result ) });
+			break;
+		case "throws=>":
+			it(vstr, function(){ expect( actualfn.apply( this, args )).toThrow( result ) });
+			break;
+    	}
+}
+
+
+// still used?
+function asVec(labels, args) { // call as ('xyz', [ args of length: 2[s, {x:,y:,z:}] 4[s, x,y,z] 3[x,y,z] or 1[{x:,y:,z:}] )
+    var s = '', v;
+    switch (args.length){
+        case 1: v = args[0]; break;
+        case 2: s = args[0]; v = args[1]; break;
+        case 3: v = { x:args[0], y:args[1], z:args[2] }; break;
+        case 4: s = args[0]; v = { x:args[1], y:args[2], z:args[3] }; break;
+        default: return 'asXYZ: invalid args';
+    }
+    return sprintf('%s{%s:%5.2f %s:%5.2f  %s:%5.2f}', s, labels.charAt(0), v.x,  labels.charAt(1), v.y, labels.charAt(2), v.z);
+}
+function asXYZ(){ return asVec('xyz', Array.prototype.slice.call(arguments));
+}
+function asABG(){ return asVec('abg', Array.prototype.slice.call(arguments));
+}
+
---
+++ tests.js
@@ -0,0 +1,177 @@
+"use strict";
+
+var Log 	= require('../bkts/js/log');
+var Utils = require('gb53_utils'), PI = Utils.PI, DEG = Utils.DEG, asBool = Utils.asBool, asNum = Utils.asNum, 
+	asInt = Utils.asInt, selBool = Utils.selBool, selNum = Utils.selNum, selInt = Utils.selInt, angleWrap = Utils.angleWrap, 
+	clamp = Utils.clamp, pctClamp = Utils.pctClamp, qindexOf = Utils.qindexOf, extract = Utils.extract, asLiteral = Utils.asLiteral, verify = Utils.verify;
+
+var expect = require('expect');
+
+describe("gb53_utils",  function() {    
+	Utils.init();
+//    describe("failures",  function() {    
+//    	//Log.i('+verify: ENABLE');
+//    	//Log.i('+extract: ENABLE');
+//    	//Log.i('+qindexOf: ENABLE');
+//    	//Log.i('+qsplit: ENABLE');
+//		verify(" extract( ' fn( a, b, c) => 0  ', '(', ')' ) => 'a, b, c' ",extract); 
+//		verify(" extract( ' fn( a, b, c) => 0  ',   0, '(' ) => 'fn' ",		extract); 
+//		verify(" extract( ' fn ( a, b, c) => 0 ',   0, '(' ) => 'fn' ",		extract); 
+//    	Log.i('-verify: DISABLE');
+//    	Log.i('-extract: DISABLE');
+//    	Log.i('-qindexOf: ENABLE');
+//    	Log.i('-qsplit: ENABLE');
+//	});
+    describe("PI",  function() {    
+    	it("it: returns Math.PI ", function(){ expect( PI ).toEqual( Math.PI ); } ); 
+	});
+    describe("DEG",  function() {    
+    	it("it: returns degrees to radians ",  function(){ expect( DEG ).toEqual( Math.PI/180.0 ) } );
+	});
+    describe("asLiteral", function(){
+		it("it:  asLiteral( 5 ) => 5 ", 			function(){ expect( asLiteral( 5		)).toEqual( 5		) });
+		it("it:  asLiteral( true ) => true ", 		function(){ expect( asLiteral( true 		)).toEqual( true 	) });
+		it("it:  asLiteral( null ) => null ", 		function(){ expect( asLiteral( null 		)).toEqual( null 	) });
+		it("it:  asLiteral( '5' ) => 5 ", 			function(){ expect( asLiteral( '5'		)).toEqual( 5 		) });
+		it("it:  asLiteral( 'true' ) => true ", 		function(){ expect( asLiteral( 'true'		)).toEqual( true	) });
+		it("it:  asLiteral( 'false' ) => false ", 		function(){ expect( asLiteral( 'false'		)).toEqual( false	) });
+		it("it:  asLiteral( 'null' ) => null ", 		function(){ expect( asLiteral( 'null'		)).toEqual( null 	) });
+	    it("it:  asLiteral( 'undefined' ) => undefined ",	function(){ expect( asLiteral( 'undefined'	)).toEqual( undefined	) });
+		it("it:  asLiteral( 'NaN' ) => NaN ", 		function(){ expect( asLiteral( 'NaN'		)).toBe(    1/0 	) });
+		it("it:  asLiteral( \" '5' \" ) => '5' ", 		function(){ expect( asLiteral( " '5' "		)).toEqual( '5' 	) });
+		it("it:  asLiteral( \"'true' \" ) => 'true' ", 	function(){ expect( asLiteral( "'true' "	)).toEqual( 'true'	) });
+		it("it:  asLiteral( \"'null'\" ) => 'null' ", 	function(){ expect( asLiteral( "'null'"		)).toEqual( 'null' 	) });
+    });
+    describe("qindexOf", function(){
+    	var arg1 = 'ABC( DEF ) G', arg2 = 'abc "def" ghi \'jkl\' def jkl';
+    	it("it: qindexOf( '" + arg1 + "', '(' ) => 3 ", function(){ expect( qindexOf(arg1, '(')).toEqual(3) });
+    	it("it: qindexOf( '" + arg1 + "', 'D' ) => 5 ", function(){ expect( qindexOf(arg1, 'D')).toEqual(5) });
+    	
+    	it("it: qindexOf( '" + arg2 + "', 'g' ) => 10 ", function(){ expect( qindexOf(arg2, 'g')).toEqual(10) });
+    	it("it: qindexOf( '" + arg2 + "', 'd' ) => 20 ", function(){ expect( qindexOf(arg2, 'd')).toEqual(20) });
+    	it("it: qindexOf( '" + arg2 + "', 'k' ) => 25 ", function(){ expect( qindexOf(arg2, 'k')).toEqual(25) });
+    });
+    describe("extract",  function() {    
+		// function extract(s, front,back){
+		it("it: extract( 'abc( def ) g',   0, '(' ) => 'abc'",    function(){ expect( extract('abc( def ) g',   0, '(' ) ).toEqual( 'abc'    ) } );
+		it("it: extract( 'abc( def ) g', '(', ')' ) => 'def'", 	  function(){ expect( extract('abc( def ) g', '(', ')' ) ).toEqual( 'def'    ) } );
+		it("it: extract( 'abc( def ) g', 'd'      ) => 'ef ) g'", function(){ expect( extract('abc( def ) g', 'd'      ) ).toEqual( 'ef ) g' ) } );
+		it("it: extract( 'abc( def ) g',   0, 'd' ) => 'abc('",   function(){ expect( extract('abc( def ) g',   0, 'd' ) ).toEqual( 'abc('   ) } );
+		
+		it("it: extract( ' fn() => 0 ', '(', ')' ) => '' ",					function(){ expect( extract(' fn() => 0 ', '(', ')' )).toEqual('')});
+		it("it: extract( ' fn(a) => 0 ', '(', ')' ) => 'a' ",				function(){ expect( extract(' fn(a) => 0 ', '(', ')' )).toEqual('a')});
+		it("it: extract( ' fn( a, b, c) => 0 ', '(', ')' ) => 'a, b, c' ",	function(){ expect( extract(' fn( a, b, c) => 0 ', '(', ')' )).toEqual('a, b, c')});
+		it("it: extract( ' fn( a, b, c) => 0 ', 0, '(' ) => 'fn' ",			function(){ expect( extract(' fn( a, b, c) => 0 ', 0, '(' )).toEqual('fn')});
+		it("it: extract( ' fn ( a, b, c) => 0 ', 0, '(' ) => 'fn' ",		function(){ expect( extract(' fn ( a, b, c) => 0 ', 0, '(' )).toEqual('fn')});
+
+		verify(" extract( 'abc( def ) g',   0, '(' ) => 'abc' ", 			extract);
+		verify(" extract( 'abc( def ) g', '(', ')' ) => 'def' ", 			extract);
+		verify(" extract( 'abc( def ) g', 'd'      ) => 'ef ) g' ", 		extract);
+		verify(" extract( 'abc( def ) g',   0, 'd' ) => 'abc(' ", 			extract);
+			
+		verify(" extract( ' fn() => 0          ', '(', ')' ) => '' ",		extract); 
+		verify(" extract( ' fn(a) => 0         ', '(', ')' ) => 'a' ",		extract); 
+		verify(" extract( ' fn( a, b, c) => 0  ', '(', ')' ) => 'a, b, c' ",extract); 
+		verify(" extract( ' fn( a, b, c) => 0  ',   0, '(' ) => 'fn' ",		extract); 
+		verify(" extract( ' fn ( a, b, c) => 0 ',   0, '(' ) => 'fn' ",		extract); 
+
+    	var arg2a = " fn( '(' ) ", res2a="'('",  arg3a = " fn( ')' ) ", res3a="')'", arg4a = " fn('\"') ", res4a="'\"'";
+    	var arg2b = ' fn( "(" ) ', res2b='"("',  arg3b = ' fn( ")" ) ', res3b='")"', arg4b = ' fn("\'") ', res4b='"\'"';
+		it('it: extract( "'+arg2a+'", "(", ")" ) => "'+res2a+'" ',  function(){ expect( extract(arg2a, '(', ')' ) ).toEqual( res2a ) } );
+		it("it: extract( '"+arg2b+"', '(', ')' ) => '"+res2b+"' ",  function(){ expect( extract(arg2b, '(', ')' ) ).toEqual( res2b ) } );
+		it('it: extract( "'+arg3a+'", "(", ")" ) => "'+res3a+'" ',  function(){ expect( extract(arg3a, '(', ')' ) ).toEqual( res3a ) } );
+		it("it: extract( '"+arg3b+"', '(', ')' ) => '"+res3b+"' ",  function(){ expect( extract(arg3b, '(', ')' ) ).toEqual( res3b ) } );
+		it('it: extract( "'+arg4a+'", "(", ")" ) => "'+res4a+'" ',  function(){ expect( extract(arg4a, '(', ')' ) ).toEqual( res4a ) } );
+		it("it: extract( '"+arg4b+"', '(', ')' ) => '"+res4b+"' ",  function(){ expect( extract(arg4b, '(', ')' ) ).toEqual( res4b ) } );
+    });
+    describe("asBool",  function() {    
+    	verify(" asBool( true ) => true ", 	 asBool );
+    	verify(" asBool( 1 ) => true ", 	 asBool );
+    	verify(" asBool( 'true' ) => true ", asBool );
+    	verify(" asBool( 'True' ) => true ", asBool );
+    	verify(" asBool( 'tRUe' ) => true ", asBool );
+    	verify(" asBool( 'T' ) => true ", 	 asBool );
+    	verify(" asBool( 'on' ) => true ", 	   asBool );
+    	verify(" asBool( 'set' ) => true ",    asBool );
+    	verify(" asBool( '1' ) => true ", 	   asBool );
+    	verify(" asBool( 'active' ) => true ", asBool );
+			
+    	verify(" asBool( false ) => false ", 		asBool );
+    	verify(" asBool( 0 ) => false ", 			asBool );
+    	verify(" asBool( 'false' ) => false ", 		asBool );
+    	verify(" asBool( 'F' ) => false ", 			asBool );
+    	verify(" asBool( 'off' ) => false ", 		asBool );
+    	verify(" asBool( 'unset' ) => false ", 		asBool );
+    	verify(" asBool( 'reset' ) => false ", 		asBool );
+    	verify(" asBool( '0' ) => false ", 			asBool );
+    	verify(" asBool( 'inactive' ) => false ", 	asBool );
+
+		verify(" asBool( 'toggle', true ) => false ", 	asBool );
+		verify(" asBool( 'toggle', false ) => true ", 	asBool );
+		verify(" asBool( 'switch', true ) => false ", 	asBool );
+		verify(" asBool( 'switch', false ) => true ", 	asBool );
+		verify(" asBool( '-', true ) => false ", 		asBool );
+		verify(" asBool( '-', false ) => true ", 		asBool );
+
+		verify(" asBool( 2 ) => 'ERR' ", 	asBool );
+		verify(" asBool( -1 ) => 'ERR' ", 	asBool );
+		verify(" asBool( 'x' ) => 'ERR' ", 	asBool );
+		verify(" asBool( '' ) => 'ERR' ", 	asBool );
+    });
+    describe("clamp", function(){
+    	verify(" clamp(5, 0, 100) => 5", clamp);
+    	verify(" clamp(-5, 0, 100) => 0", clamp);
+    	verify(" clamp(105, 0, 100) => 100", clamp);
+		verify(" clamp(45, 40, 50) => 45", clamp);
+		verify(" clamp(10, 40, 50) => 40", clamp);
+		verify(" clamp(72, 40, 50) => 50", clamp);
+		verify(" clamp(-37, -40, -20) => -37", clamp);
+		verify(" clamp(-57, -40, -20) => -40", clamp);
+		verify(" clamp( 17, -40, -20) => -20", clamp);
+    });
+    describe("pctClamp", function(){
+    	verify(" pctClamp(30, 0,100) => 30 ", pctClamp);
+		verify(" pctClamp(30, 0,100) => 30 ", pctClamp);
+		verify(" pctClamp(10, 40,60) => 42 ", pctClamp);
+		verify(" pctClamp(50, 40,60) => 50 ", pctClamp);
+		verify(" pctClamp(-10, 40,60) => 40 ", pctClamp);
+		verify(" pctClamp(1010, 40,60) => 60 ", pctClamp);
+		verify(" pctClamp(50, -40,40) => 0 ", pctClamp);
+		verify(" pctClamp(25, -40,40) => -20 ", pctClamp);
+		verify(" pctClamp(75, -40,40) => 20 ", pctClamp);
+		verify(" pctClamp(50, -100,-40) => -70 ", pctClamp);
+		verify(" pctClamp(25, -100,-40) => -85 ", pctClamp);
+		verify(" pctClamp(75, -100,-40) => -55 ", pctClamp);
+    });
+    describe("angleWrap", function(){
+    	// function angleWrap(deg): collapse angle in degrees to [0..360)
+    	verify(" angleWrap(60) => 60 ", 	angleWrap);
+    	verify(" angleWrap(420) => 60 ", 	angleWrap);
+    	verify(" angleWrap(780) => 60 ",	angleWrap);
+    	verify(" angleWrap(-60) => 300 ", 	angleWrap);
+    	verify(" angleWrap(-420) => 300 ", 	angleWrap);
+    	verify(" angleWrap(-780) => 300 ", 	angleWrap);
+    	verify(" angleWrap(360) => 0 ", 	angleWrap);
+    });
+    describe("asNum",  function() {    
+		// function asNum(val, def_value, min, max, asInt){
+		verify(" asNum( 1.7 ) => 1.7 ", asNum );
+		verify(" asNum( '1.75' ) => 1.75 ", asNum );
+		verify(" asNum( 30.5, 50, 0, 100 ) => 30.5 ", asNum );
+		verify(" asNum( '3x.5', 50, 0, 100 ) => 50 ", asNum );
+		verify(" asNum( '', 50, 0, 100 ) => 50 ", asNum );
+		verify(" asNum( -5, 50, 0, 100 ) => 0 ", asNum );
+		verify(" asNum( 105, 50, 0, 100 ) => 100 ", asNum );
+    });
+    describe("asInt",  function() {    
+		// function asInt(val, def_value, min, max, asInt){
+		verify(" asInt( 1.7 ) => 1 ", asInt );
+		verify(" asInt( '1.75' ) => 1 ", asInt );
+		verify(" asInt( 30.5, 50, 0, 100 ) => 30 ", asInt );
+		verify(" asInt( '3x.5', 50, 0, 100 ) => 50 ", asInt );
+		verify(" asInt( '', 50, 0, 100 ) => 50 ", asInt );
+		verify(" asInt( -5, 50, 0, 100 ) => 0 ", asInt );
+		verify(" asInt( 105, 50, 0, 100 ) => 100 ", asInt );
+    });
+});
+
